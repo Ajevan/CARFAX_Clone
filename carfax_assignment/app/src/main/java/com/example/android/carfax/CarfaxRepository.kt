ace9e256e66a74bdfa452cc636763e8683edc9be
ajevan_m <ajevan.mahadaya@ontariotechu.net>
2022-03-07 22:21:46 -0500

    fix: bug fix for offline model
    bug fix for offline model

--- carfax_assignment/app/src/main/java/com/example/android/carfax/CarfaxRepository.kt
+++ carfax_assignment/app/src/main/java/com/example/android/carfax/CarfaxRepository.kt
@@ -1,9 +1,11 @@
 package com.example.android.carfax
 
+import android.util.Log
 import com.example.android.carfax.network.CarListing
 import com.example.android.carfax.network.CarfaxAPIService
 import com.example.android.carfax.offline.CarfaxDAO
 import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
 
 class CarfaxRepository(val carfaxAPI: CarfaxAPIService, val carfaxDAO: CarfaxDAO) {
 
@@ -20,16 +22,19 @@ class CarfaxRepository(val carfaxAPI: CarfaxAPIService, val carfaxDAO: CarfaxDAO
 
     fun getListingsFromApi(): Observable<List<CarListing>> {
         return carfaxAPI.getData()
-            .map { a -> a.listings }
-            .doOnNext {x ->
-                storeListingsInDb(x)
+            .map { data -> data.listings }
+            .doOnNext {listings ->
+                storeListingsInDb(listings)
             }.onErrorReturn {
                 listOf()
             }
     }
 
     fun storeListingsInDb(data: List<CarListing>) {
-        val insertDataIntoDB = Observable.fromCallable { carfaxDAO.insertAll(data) }
+        val insertDataIntoDB = Observable.fromCallable { carfaxDAO.insertAll(data) }.subscribeOn(
+            Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe()
     }
 
 }
